37fd0a784bafd38900674a9d88c4df7c2e3f60a0
jotavede <jvictordagostini@gmail.com>
2026-06-15 19:05:54 -0300

    Minhas alterações no TrabalhoFinal

---
+++ TrabalhoFinal.java
@@ -0,0 +1,22 @@
+public class TrabalhoFinal {
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao Jogo da Ilha dos Tesouros!");
+        System.out.println("Você está em uma ilha misteriosa e precisa encontrar o tesouro escondido.");
+        
+        //OP1
+        System.out.println("Instruções do  Jogo:");
+        System.out.println(
+                "1. Cada posição do jogador é representada por um vetor, onde, em cada posição, pode conter um tesouro, uma armadilha ou estar vazia.");
+        System.out.println("2. Os tipos de tesouros existentes são: Ouro, Diamante e Rubi.");
+        System.out.println("3. O jogador pode cair nas seguintes armadilhas: Buraco, Cobra e Espinhos.");
+        System.out.println(
+                "4. O objetivo do jogador é explorar posições da ilha, encontrar tesouros, evitar armadilhas e fazer a maior pontuação possível antes que as tentativas acabem.");
+
+
+
+        //OP3
+        System.out.println("\nVamos começar a aventura! Boa sorte!");
+
+        
+    }
+}
