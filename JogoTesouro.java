25839e1bc9359407c7c6bea0ba9bed367bc7c1a3
Ruan Castro <ruancastro904@gmail.com>
2026-06-15 19:35:05 -0300

    Apresentação do jogo

--- JogoTesouro.java
+++ JogoTesouro.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class JogoTesouro {
 
     public JogoTesouro() {
-        String opcoes[] = new String[10];
+        Scanner teste = new Scanner(System.in);
+        String armadilhas[] = new String[3];
+        String tesouros[] = new String[3];
+        int opcao;
 
+        do {
+            System.out.println("===== ILHA DOS TESOUROS =====\r\n" + //
+                    "1 - Mostrar instruções\r\n" + //
+                    "2 - Mostrar mapa\r\n" + //
+                    "3 - Jogar\r\n" + //
+                    "4 - Mostrar status\r\n" + //
+                    "5 - Sair\r\n" + //
+                    "Escolha uma opção:");
+            opcao = teste.nextInt();
+            System.out.println("Sai");
+
+        } while (opcao != 4);
+    }
+
+    public static void main(String[] args) {
+        new JogoTesouro();
     }
 
 }
