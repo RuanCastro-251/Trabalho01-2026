4b940ef34eb7e0fe20a3d481729142cb839ce17a
jotavede <jvictordagostini@gmail.com>
2026-06-15 21:19:54 -0300

    Alterações 21:19

--- JogoTesouro.java
+++ JogoTesouro.java
@@ -15,7 +15,7 @@ public class JogoTesouro {
         int opcao;
 
         do {
-            
+
             System.out.println("===== ILHA DOS TESOUROS =====\r\n" + //
                     "1 - Mostrar instruções\r\n" + //
                     "2 - Mostrar mapa\r\n" + //
@@ -27,13 +27,14 @@ public class JogoTesouro {
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Instruções do  Jogo:");
+                    System.out.println("\n=== INSTRUÇÕES DO JOGO ===");
+                    System.out.println("- Escolha uma posição de 0 a 14 para explorar na ilha.");
+                    System.out.println("- Tesouros disponíveis: OURO (+10), DIAMANTE (+20), RUBI (+15).");
+                    System.out.println("- Armadilhas perigosas: BURACO (-5), COBRA (-10), ESPINHOS (-7).");
+                    System.out.println("- Posições do tipo VAZIO não alteram sua pontuação.");
+                    System.out.println("- Você tem um limite máximo de 8 jogadas.");
                     System.out.println(
-                            "1- Cada posição do jogador é representada por um vetor, onde, em cada posição, pode conter um tesouro, uma armadilha ou estar vazia.\n");
-                    System.out.println("2- Os tipos de tesouros existentes são: Ouro, Diamante e Rubi.\n");
-                    System.out.println("3- O jogador pode cair nas seguintes armadilhas: Buraco, Cobra e Espinhos.\n");
-                    System.out.println(
-                            "4- O objetivo do jogador é explorar posições da ilha, encontrar tesouros, evitar armadilhas e fazer a maior pontuação possível antes que as tentativas acabem.\n");
+                            "- O jogo acaba se as tentativas zerarem ou se todos os tesouros escondidos forem descobertos.");
 
                     break;
 
@@ -51,26 +52,5 @@ public class JogoTesouro {
             mapa[i] = "?";
             System.out.println("[" + i + "]" + mapa[i]);
         }
-        // if();
     }
-
-    private static String sortear() {
-        String tipos[] = { "OURO", "DIAMANTE", "RUBI", "BURACO", "COBRA", "ESPINHOS", "VAZIO" };
-
-        int posicao = (int) (Math.random() * 7);
-
-        return tipos[posicao];
-    }
-
-    static void exibirBoasVindas() {
-        System.out.println("Seja Bem-vindo ao jogo Ilha dos Tesouros!\n");
-        System.out.println("A ilha possui 15 posições, que são numeradas de 0 a 14.");
-        System.out.println("Você possui 8 tentativas para encontrar os tesouros.");
-
-    }
-
-    public static void main(String[] args) {
-        new JogoTesouro();
-    }
-
 }
